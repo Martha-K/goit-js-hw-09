4e1893dedde2f952592633abf7185f75aed976ea
Martha-K <110817385+Martha-K@users.noreply.github.com>
2023-04-03 11:22:07 +0000

    Deploying to gh-pages from @ Martha-K/goit-js-hw-09@22014fa936e5d3441ec98492e54233986c8d8124 🚀

---
+++ 03-promises.7ad55235.js
@@ -0,0 +1,2 @@
+function e(e,t){const n=Math.random()>.3;return new Promise(((o,r)=>{setTimeout((()=>{n?o([e,t]):r([e,t])}),t)}))}document.querySelector(".form").addEventListener("submit",(function(t){t.preventDefault();const{elements:{delay:n="",step:o="",amount:r=""}}=t.currentTarget||{},u=Number(n.value),l=Number(o.value),m=Number(r.value);for(let t=0;t<=m-1;t++){e(t,l*t+u).then((([e,t])=>{console.log(`✅ Fulfilled promise ${e+1} in ${t} ms`)})).catch((([e,t])=>{console.log(`❌ Rejected promise ${e+1} in ${t} ms`)}))}}));
+//# sourceMappingURL=03-promises.7ad55235.js.map
